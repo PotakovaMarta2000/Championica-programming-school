8bdd8a3a8ffc43a9f0bf36498e45fffc05050b55
PotakovaMarta2000 <potakovamarta@mail.ru>
2022-06-13 20:40:39 +0300

    work with intervals

--- interval_timer/main.js
+++ interval_timer/main.js
@@ -1,1 +1,24 @@
-// Создаем интервал
+// Бесконечный интервал
+// Cоздаем переменную counter
+// let counter = 0;
+// // Создаем интервал. безымянную функцию
+// setInterval(function()  {
+// counter++;
+// console.log("Прошло секунд: " + counter); 
+// }, 1000);
+
+// Cоздаем переменную counter
+let counter = 0;
+
+// Присваиваем id setInterval
+let id = setInterval(my_func, 1000);
+
+// Прописываем функцию
+function my_func() {
+    counter++;
+    console.log("Прошло секунд: " + counter);
+
+// Прописываем условия внутри функции, очищаем интервал
+    if(counter == 3)
+    clearInterval(id);
+}
